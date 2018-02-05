60e0cf1276caaa6763828510db131b9ca376c4a3
Electromaster232 <gilbert7573@gmail.com>
2018-02-04 19:34:46 -0500

    WOOHOO! Let's get started!
    Added all files needed, have to write the .gitignore and the Wiki/documentation!

---
+++ keygen.py
@@ -0,0 +1,9 @@
+import secrets
+
+print("Copyright 2018 DJ-Electro.me. This software is part of the Flask-powered dj-electro.me file hosting site.")
+print("How many bits to generate? IMPORTANT: 32-bit minimum for security.")
+bits = input()
+bitsint = int(bits)
+key = secrets.token_urlsafe(bitsint)
+print("Here is your key, ensure you add it to the database!")
+print(key)
---
+++ schema.sql
@@ -0,0 +1,13 @@
+--
+-- File generated with SQLiteStudio v3.1.1 on Sun Feb 4 16:48:40 2018
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: key
+CREATE TABLE "key" (allowed_keys STRING PRIMARY KEY);
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
